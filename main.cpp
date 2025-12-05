0d8de377d2327e1cdcb74551be29d92df2fd08d0
Egor <eip_spb@mail.ru>
2025-12-05 12:44:07 +0300

    struct Dot

--- main.cpp
+++ main.cpp
@@ -11,6 +11,11 @@ namespace topit {
     virtual p_t begin() const = 0;
     virtual p_t next(p_t) const = 0;
   };
+
+  struct Dot : IDraw {
+    p_t begin() const override;
+    p_t next(p_t) const override;
+  };
 }
 
 int main() {
