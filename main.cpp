c60b2433b182b5685d8b8b87d5e3efd52e2fcac4
Egor <eip_spb@mail.ru>
2025-12-05 13:00:13 +0300

    more Dot

--- main.cpp
+++ main.cpp
@@ -23,19 +23,18 @@ namespace topit {
 
 int main() {
   using topit::p_t;
-  p_t a{1, 0}, b{1, 0};
-  std::cout << (a != b) << "\n";
 }
-
-topit::Dot(int x, int y):
+topit::Dot::Dot(p_t dd):
+  IDraw(),
+  d{dd}
+{}
+topit::Dot::Dot(int x, int y):
   IDraw(),
   d{x, y}
 {}
-
 topit::p_t topit::Dot::begin() const {
   return d;
 }
-
 topit::p_t topit::Dot::next(p_t prev) const {
   if (prev != begin()) {
     throw std::logic_error("bad impl");
